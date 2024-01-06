1bb050ebb4de29fbb7b49d6fde7b67151951c45f
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-01-06 16:04:19 +0530

    tree traversals - preopder, inorder, postorder

---
+++ 06 Apna College/01 TREE/02BinaryTreeTraversals.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preOrder(struct Node *root)
+{ // RootNode | LeftSubtree | RightSubtree
+
+    if (root == NULL)
+    {
+        return;
+    };
+
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+};
+
+void inOrder(struct Node *root)
+{ // LeftSubtree | RootNode | RightSubtree
+
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+};
+
+void postOrder(struct Node *root)
+{ // LeftSubtree | RightSubtree | RootNode
+
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    cout << "PreOrder Tree Traversal" << endl;
+    preOrder(root);
+    cout << endl;
+
+    cout << "InOrder Tree Traversal" << endl;
+    inOrder(root);
+    cout << endl;
+
+    cout << "PostOrder Tree Traversal" << endl;
+    postOrder(root);
+    cout << endl;
+    return 0;
+};
